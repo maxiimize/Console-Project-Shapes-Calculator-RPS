66157a0008fb986b8f113eb71f69b06c212e7246
maxiimize <max.b@hotmail.se>
2025-05-22 18:25:52 +0200

    Add Rectangle class inheriting from Shape
    Introduces a new `Rectangle` class with required properties `Width` and `Height`. The abstract `Shape` class is defined with required properties for `Id`, `Area`, `Perimeter`, and `DateCreated`, establishing a foundation for geometric shapes in the application.

---
+++ DataAcessLayer/Models/Rectangle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAcessLayer.Models
+{
+    public class Rectangle : Shape
+    {
+        [Required]
+        public double Width { get; set; }
+
+        [Required]
+        public double Height { get; set; }
+    }
+}
---
+++ DataAcessLayer/Models/Shape.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAcessLayer.Models
+{
+    public abstract class Shape
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public double Area { get; set; }
+
+        [Required]
+        public double Perimeter { get; set; }
+
+        [Required]
+        public DateTime DateCreated { get; set; }
+    }
+}
