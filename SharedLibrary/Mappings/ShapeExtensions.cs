9308773e0f97cdd39f7d68cf2e975316958a9919
maxiimize <max.b@hotmail.se>
2025-06-06 16:09:58 +0200

    Add ShapeExtensions with ToViewModel method
    Introduces a new static class `ShapeExtensions` in the `SharedLibrary.Mappings` namespace. This class includes an extension method `ToViewModel` for converting `Shape` objects into `ShapeViewModel` instances. The method utilizes a switch expression to handle different shape types and constructs a detailed string representation of their parameters, returning a fully populated `ShapeViewModel`.

---
+++ SharedLibrary/Mappings/ShapeExtensions.cs
@@ -0,0 +1,34 @@
+﻿using DataAcessLayer.ModelsShapes;
+using SharedLibrary.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.Mappings
+{
+    public static class ShapeExtensions
+    {
+        public static ShapeViewModel ToViewModel(this Shape s)
+        {
+            string parameters = s switch
+            {
+                Rectangle r => $"W={r.Width}, H={r.Height}",
+                Parallelogram p => $"B={p.BaseLength}, S={p.SideLength}, H={p.Height}",
+                Triangle t => $"Base={t.BaseLength}, H={t.Height}, S1={t.SideA}, S2={t.SideB}",
+                Rhombus h => $"S={h.SideLength}, H={h.Height}",
+                _ => string.Empty
+            };
+            return new ShapeViewModel
+            {
+                Id = s.Id,
+                ShapeType = s.GetType().Name,
+                Parameters = parameters,
+                Area = s.Area,
+                Perimeter = s.Perimeter,
+                DateCreated = s.DateCreated.ToString("yyyy-MM-dd HH:mm")
+            };
+        }
+    }
+}
