c3788915127b273a86502fc9e44a5d6de281e58c
maxiimize <max.b@hotmail.se>
2025-06-06 10:43:46 +0200

    Add CalculatorViewModel class for calculator state
    Introduces a new `CalculatorViewModel` class with properties
    for `Id`, `Operand1`, `Operand2`, `Operator`, `Result`,
    and `DateCreated` to represent the state of a calculator
    operation in a structured manner.

---
+++ SharedLibrary/ViewModels/CalculatorViewModel.cs
@@ -0,0 +1,9 @@
+﻿public class CalculatorViewModel
+{
+    public int Id { get; set; }
+    public double Operand1 { get; set; }
+    public double? Operand2 { get; set; }
+    public string Operator { get; set; }
+    public double Result { get; set; }
+    public string DateCreated { get; set; }
+}
