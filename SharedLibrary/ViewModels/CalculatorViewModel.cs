bf7c5670be626631e1ba42d1265b5a5f089d1459
maxiimize <max.b@hotmail.se>
2025-06-06 10:47:13 +0200

    Add RpsViewModel
    Added a new RpsViewModel.cs file to manage rock-paper-scissors game properties, including player and computer moves, outcome, win rate, and date played.

--- SharedLibrary/ViewModels/CalculatorViewModel.cs
+++ SharedLibrary/ViewModels/CalculatorViewModel.cs
@@ -1,9 +1,18 @@
-﻿public class CalculatorViewModel
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.ViewModels
 {
-    public int Id { get; set; }
-    public double Operand1 { get; set; }
-    public double? Operand2 { get; set; }
-    public string Operator { get; set; }
-    public double Result { get; set; }
-    public string DateCreated { get; set; }
+    public class CalculatorViewModel
+    {
+        public int Id { get; set; }
+        public double Operand1 { get; set; }
+        public double? Operand2 { get; set; }
+        public string Operator { get; set; }
+        public double Result { get; set; }
+        public string DateCreated { get; set; }
+    }
 }
---
+++ SharedLibrary/ViewModels/RpsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.ViewModels
+{
+    public class RpsViewModel
+    {
+        public int Id { get; set; }
+        public string PlayerMove { get; set; }
+        public string ComputerMove { get; set; }
+        public string Outcome { get; set; }
+        public decimal WinRate { get; set; }
+        public string DatePlayed { get; set; }
+    }
+}
